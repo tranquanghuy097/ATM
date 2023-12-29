101ce5d43794dfc80fd452891e91e62873c57fd5
Huy <tranquanghuy097@gmail.com>
2023-12-29 16:31:30 +0700

    Add check balance test

--- transaction_services/tests/test_transaction.py
+++ transaction_services/tests/test_transaction.py
@@ -14,3 +14,14 @@ def test_check_balance(setup_teardown_read,
         code=expected.code
     ).execute()
     assert result == expected
+
+
+def test_check_balance_not_found(setup_teardown_read,
+                                 mock_account_read,
+                                 session_maker):
+    result = worker.TransactionWorker(
+        state=worker.TransactionType.CHECK_BALANCE,
+        session_maker=session_maker,
+        code="00000000"
+    ).execute()
+    assert result == None   # noqa: E711
